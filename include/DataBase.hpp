74b2799886b5017708346d03698abc4772e90862
yin <1206489318@qq.com>
2017-11-18 02:02:20 +0800

    完成DataBase部分的三个api

--- include/DataBase.hpp
+++ include/DataBase.hpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-//数据库的大小是1550kb，约1.5M内存,但是实际测试显示内存占用为0.3M 
+//数据库的大小是1550kb，约1.5M内存,但是实际测试显示内存占用为0.3M
 class DataBase{
 private:
   vector<PosePoint> m_diindex;//正向索引0-46655,729kb
@@ -22,7 +22,7 @@ public:
   void search(PosePoint& pp,vector<PosePointID>& numlist);
   //match API
   bool ismatch_posepoint(const PosePointID& ppid1,const PosePointID& ppid2);
-  bool ismatch_stack(const PosePointID& ppid,const vector<PosePointID> & magicstack);//这个magicstack可能要用std::map<NumID,PosePointID> 
+  bool ismatch_stack(const PosePointID& ppid,const vector<PosePointID> & ppid_stack);//这个magicstack可能要用std::map<NumID,PosePointID>
 };
 
 DataBase::DataBase()
@@ -52,7 +52,7 @@ void DataBase::create_allindex()
     //创建正向索引
     this->m_diindex.push_back(PosePoint(line));
     //创建反向索引
-    this->m_inindex.resize(81);//必须初始化 
+    this->m_inindex.resize(81);//必须初始化
     for(int i=0;i<line.size();i++)
       if(line[i]=='1')
         this->m_inindex[i].push_back(ppid);
@@ -64,40 +64,51 @@ void DataBase::create_allindex()
 
 bool DataBase::ismatch_posepoint(const PosePointID& ppid1,const PosePointID& ppid2)
 {
-	//进行按位与操作，双1则冲突，和谐则0，若全0则匹配 
-	if(((this->m_diindex[ppid1])&(this->m_diindex[ppid2]))==PosePoint(0))//还可以用none，any等函数 
+  //首先进行用户输入检查
+  if(ppid1<0 || ppid2<0 || ppid1>46655 || ppid2>46655 ) cerr<<"[Error]:PosePointID is not valid!"<<endl;
+	//进行按位与操作，双1则冲突，和谐则0，若全0则匹配
+	if(((this->m_diindex[ppid1])&(this->m_diindex[ppid2]))==PosePoint(0))//还可以用none，any等函数
 		return true;
 	else
 		return false;
-} 
+}
+
+bool DataBase::ismatch_stack(const PosePointID& ppid,const vector<PosePointID>& ppid_stack )
+{
+  for(int i=0;i < ppid_stack.size();i++)
+  {
+    if(!ismatch_posepoint(ppid,ppid_stack[i])) return false;
+  }
+  return true;
+}
 
 
 void DataBase::search(PosePoint& pp,vector<PosePointID>& numlist)
 {
-	if(pp==PosePoint(0))//先处理全0的特殊情况 
+	if(pp==PosePoint(0))//先处理全0的特殊情况
 	{
 		for(PosePointID ppid=0;ppid<46656;ppid++)
 			numlist.push_back(ppid);
 		return ;
 	}
 	int count=0;
-	
+
 	vector<PosePointID> tmp;
 	vector<PosePointID>::iterator itend;
 	for(size_t i=0;i<pp.size();i++ )
 	{
-		if( pp.test(i) ) 
+		if( pp.test(i) )
 		{++count;
 			if(count==1){
 				tmp.insert(tmp.end(),(this->m_inindex[i]).begin(),(this->m_inindex[i]).end());
-				itend = tmp.end();	
-			}		
+				itend = tmp.end();
+			}
 			else{
 				itend = set_intersection(tmp.begin(), itend, (this->m_inindex[i]).begin(), (this->m_inindex[i]).end(), tmp.begin());
 			}
 		}
 	}
-	
+
 	numlist.insert(numlist.end(),tmp.begin(),itend);
 }
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,49 @@
 
 using namespace std;
 
+#define TEST_API_2
+
 int main()
 {
-	
-	DataBase db;
-	
-  cout<<db.ismatch_posepoint(46275,46275)<<endl;
-  vector<PosePointID> numlist;
-  PosePoint a(string("000000000000000000000000000000000000000000000000000000000000000000000000000000000"));
-  db.search(a,numlist);
 
+	DataBase db;
+
+	#ifdef TEST_API_1
+	//测试 ismatch_posepoint 函数
+	cout<<"testing function ismatch_posepoint()......"<<endl;
+	PosePointID ppid1=0,ppid2=888;
+	if(db.ismatch_posepoint(ppid1,ppid2))
+  	cout<<"PosePointID "<<ppid1<<" and "<<"PosePointID "<<ppid2<<" is match!"<<endl;
+	else
+		cout<<"PosePointID "<<ppid1<<" and "<<"PosePointID "<<ppid2<<" is not match!"<<endl;
+	#endif
+
+	#ifdef TEST_API_2
+	//测试 ismatch_stack 函数
+	cout<<"testing function ismatch_stack()......"<<endl;
+	vector<PosePointID> ppidstatck;
+	PosePointID q_ppid=0;
+	ppidstatck.push_back(1);
+	ppidstatck.push_back(2);
+	ppidstatck.push_back(3);
+	cout<<"PosePointID "<<q_ppid<<" vs:"<<endl;
+	for(int i=0;i<ppidstatck.size();i++)
+		cout<<ppidstatck[i]<<endl;
+	if(db.ismatch_stack(q_ppid,ppidstatck))
+		cout<<"match stack!"<<endl;
+	else
+		cout<<"not match stack!"<<endl;
+	#endif
+
+	#ifdef TEST_API_3
+	//测试	search 函数
+	cout<<"testing function search()......"<<endl;
+	vector<PosePointID> numlist;
+  PosePoint a(string("100000000000000000000000000000000000000000000000000000000000000000000000000000000"));
+	cout<<"now searching temple "<<a<<endl;
+  db.search(a,numlist);
+	cout<<"there are "<<numlist.size()<<" matched PosePoint:"<<endl;
+	for(int i=0;i<numlist.size();i++)
+	cout<<numlist[i]<<endl;
+	#endif
 }
